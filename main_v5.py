eddac631a4c76f016982a42c97b8bdd9f4102277
IDGS-801-18002424 <18002424@alumnos.utleon.edu.mx>
2023-01-30 19:42:20 -0600

    Clase 30/01/23.

---
+++ main_v5.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('calcular.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
